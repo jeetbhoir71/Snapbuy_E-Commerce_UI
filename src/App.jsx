3e5c52ecffeb56fc0aba23f0eb17a03af8e6ac64
jeetbhoir71 <jeetbhoir2024@gmail.com>
2026-06-22 09:22:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Products from './pages/Products.jsx'
-import Cart from './pages/Cart.jsx'
-import About from './pages/About.jsx'
-import Login from './pages/Login.jsx'
+import Home from './Pages/Home.jsx'
+import Products from './Pages/Products.jsx'
+import Cart from './Pages/Cart.jsx'
+import About from './Pages/About.jsx'
+import Login from './Pages/Login.jsx'
 import BecomeSeller from './Pages/BecomeSeller.jsx'
 
 function App() {
